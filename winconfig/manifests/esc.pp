5c3ffb5a1d69540ca1db08360a5c8752d63b401c
Matthew Stone <matt@souldo.net>
2014-03-10 09:24:51 -0700

    ie_esc cleanup
    Changed registry_value to registry::value.

--- winconfig/manifests/esc.pp
+++ winconfig/manifests/esc.pp
@@ -3,19 +3,15 @@ define winconfig::esc(
   $ensure,
 ) {
   include winconfig::params
-  case $ensure { 
+  case $ensure {
     'present','enabled': { $esc_data = 1 }
     'absent','disabled': { $esc_data = 0 }
     default: { fail('You must specify ensure status...') }
-  } 
-
-  registry_value { 'hklm\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}\IsInstalled':
+  }
+  registry::value { 'IE ESC':
+    key    => 'hklm\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}',
+    value  => 'IsInstalled',
     type   => dword,
     data   => $esc_data,
   }
-
-  registry_key  { 'hklm\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}':
-  ensure => present, 
-      }
 }
-  
