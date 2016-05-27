fa7caff1f841c36b2e83b3b8df99926dabdb94ee
Matthew Stone <Matthew Stone>
2016-05-27 10:45:30 -0700

    fixed ps version

--- lib/facter/win_powershell_version.rb
+++ lib/facter/win_powershell_version.rb
@@ -3,7 +3,7 @@ Facter.add("win_dotnet4_version") do
 
   setcode do
     ps_exec  = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
-    ps_cmd   = '($PSVersionTable.Version).Major'
+    ps_cmd   = '($PSVersionTable.PSVersion).Major'
 
     Facter::Util::Resolution.exec(%Q{#{ps_exec} -command #{ps_cmd}}).strip
   end
